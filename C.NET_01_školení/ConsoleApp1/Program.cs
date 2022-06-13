e79d27048a299ce9120dc78ac09fee8e9bccb22e
eycis <dzugam00@jcu.cz>
2022-06-13 15:57:46 +0200

    linq metody

--- C.NET_01_školení/ConsoleApp1/Program.cs
+++ C.NET_01_školení/ConsoleApp1/Program.cs
@@ -1,28 +1,62 @@
 ﻿using Data;
 using model;
+using System.Linq;
 
 Console.WriteLine("Hello, World!");
-//nalezení cesty k souborům
-var booksdir = @"C:\Users\marie\source\repos\NewRepo\C.NET_01_školení\books";
-//načtení všech textových souborů
-var files = Directory.EnumerateFiles(booksdir, "*.txt");
 
-foreach (var file in files)
+int[] numbers = { 11, 2, 13, 44, -5, 6, 127, -99, 0, 256};
+//linq WHERE
+//var result = numbers.Where(n => n > 0) //využívání linqu k filtraci podle zadaných podmínek v závorkách
+//                    .Where(n => n < 99);
+
+//linq ORDERBY
+//var result = numbers.OrderBy(n => n); //pouze řadí od nejmenšího po největší
+
+//linq AGREGACE-
+//pro výpis max, min, avg, count, length a dalších
+//var number = numbers.Max();
+
+//linq TAKE SKIP+while
+//take bere čísla dokud je splněna podmínka, skip přeskakuje dokud platí podmínka
+//var result = numbers.TakeWhile(n => n > 0);
+
+//linq SELECT-transformace (v tomto případě převede negativní čísla do absolutní hodnoty
+//var result = numbers.Select(n => Math.Abs(n));
+
+//string.join je opak splitu, dá mi dohromady elementy z result a oddělí je čárkou
+//první parametr je jak to chceme oddělit a druhý je jaký seznam teda máme zpracovat
+
+//Console.WriteLine(string.Join(",", result));
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+static void FreqWords()
 {
-    var result = FreqAnalysis.FreqAnalysisFromFile(file);
+    //nalezení cesty k souborům
+    var booksdir = @"C:\Users\marie\source\repos\NewRepo\C.NET_01_školení\books";
+    //načtení všech textových souborů
+    var files = Directory.EnumerateFiles(booksdir, "*.txt");
 
-    var fileInfo = new FileInfo(file);
-    Console.WriteLine(fileInfo.Name);
-
-    //tisk deseti nejčastějších slov:
-    var ordered = result.Words.OrderByDescending(kv => kv.Value).Take(10);
-    //pomocí linq může využít metodu orderbydescending, která dokáže seřadit result podle počtu výskytů. 
-    //následně metoda take vezme prvních deset.
-
-
-    //metoda pro výpis
-    foreach (var item in ordered)
+    foreach (var file in files)
     {
-        Console.WriteLine($"{item.Key}\t{item.Value}");
+        var result = FreqAnalysis.FreqAnalysisFromFile(file);
+
+        var fileInfo = new FileInfo(file);
+        Console.WriteLine(fileInfo.Name);
+
+        //tisk deseti nejčastějších slov:
+        var ordered = result.Words.OrderByDescending(kv => kv.Value).Take(10);
+        //pomocí linq může využít metodu orderbydescending, která dokáže seřadit result podle počtu výskytů. 
+        //následně metoda take vezme prvních deset.
+
+
+        //metoda pro výpis
+        foreach (var item in ordered)
+        {
+            Console.WriteLine($"{item.Key}\t{item.Value}");
+        }
     }
 }
--- C.NET_01_školení/Data/FreqAnalysis.cs
+++ C.NET_01_školení/Data/FreqAnalysis.cs
@@ -1,37 +1,60 @@
-﻿namespace Data
+﻿using model;
+
+namespace Data
 {
     public static class FreqAnalysis
     {
         public static Dictionary<string, int> FreqAnalysisFromString(string input)
         {
             Dictionary<string, int> result = new Dictionary<string, int>();
-            List<string> words = input.Split(new string[] { @",.;:()""[]", }, StringSplitOptions.None).ToList();
+            var words = input.Replace(Environment.NewLine, " ")
+                             .Replace("(", "")
+                             .Replace(")", "")
+                             .Replace(".", "")
+                             .Replace(",", " ")
+                             .Replace(":", " ")
+                             .Split();
 
             foreach (var word in words)
             {
                 if (result.ContainsKey(word))
+                {
                     result[word]++;
+                }
                 else
-                    result[word] = 1;
+                {
+                    result.Add(word, 1);
+                }
             }
-
             return result;
         }
 
-        public static async Task<Dictionary<string, int>> FreqAnalysisFromUrl(string url)
+        public static async Task<FAResult> FreqAnalysisFromUrl(string url)
         {
             //get content from url
-            HttpClient httpClient = new ();
+            HttpClient httpClient = new HttpClient();
             var content = await httpClient.GetStringAsync(url);
+            var dict =  FreqAnalysisFromString(content);
 
-            return FreqAnalysisFromString(content);
+            return new FAResult()
+            {
+                Source = url,
+                SourceType = SourceType.URL,
+                Words = dict
+            };
         }
-        public static Dictionary<string, int> FreqAnalysisFromFile(string file)
+        public static FAResult FreqAnalysisFromFile(string file)
         {
-            //todo get content from file
+            //get content from file
             var content = File.ReadAllText(file);
+            var dict = FreqAnalysisFromString(content);
 
-            return FreqAnalysisFromString(content);
+            return new FAResult()
+            {
+                Source = file,
+                SourceType = SourceType.FILE,
+                Words = dict
+            };
         }
     }
 }
