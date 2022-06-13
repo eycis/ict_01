b4e0c2f5d2562ab9dcf6b40ad9bc2ff55de3e434
eycis <dzugam00@jcu.cz>
2022-06-13 13:39:49 +0200

    freqanalysis

---
+++ C.NET_01_školení/Data/freqAnalysis.cs
@@ -0,0 +1,26 @@
+﻿namespace Data
+{
+    public static class FreqAnalysis
+    {
+        public static Dictionary<string, int> FreqAnalysisFromString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static async Task<Dictionary<string, int>> FreqAnalysisFromUrl(string url)
+        {
+            //get content from url
+            HttpClient httpClient = new ();
+            var content = await httpClient.GetStringAsync(url);
+
+            return FreqAnalysisFromString(content);
+        }
+        public static Dictionary<string, int> FreqAnalysisFromFile(string file)
+        {
+            //todo get content from file
+            var content = File.ReadAllText(file);
+
+            return FreqAnalysisFromString(content);
+        }
+    }
+}
