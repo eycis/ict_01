5d2709f97f1ace4b035b32f84767b6508cd04a8e
eycis <dzugam00@jcu.cz>
2022-06-13 11:49:35 +0200

    uprava modelu

--- C.NET_01_školení/model/FAResult.cs
+++ C.NET_01_školení/model/FAResult.cs
@@ -5,6 +5,26 @@
     /// </summary>
     public class FAResult
     {
+        /// <summary>
+        /// zdroj textu 
+        /// </summary>
+        public string Source { get; set; }
 
+        public SourceType SourceType { get; set; }
+        /// <summary>
+        /// výsledná frekvenční analýza slov
+        /// </summary>
+        public Dictionary<string, int> Words { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Source} {Words?.Count}";
+        }
     }
+    public enum SourceType
+    {
+        URL, 
+        FILE
+    }
+
 }
