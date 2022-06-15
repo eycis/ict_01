17304255c6b8eee9c04ea9c8dc6d521d6bf30780
eycis <dzugam00@jcu.cz>
2022-06-15 14:29:57 +0200

    controllers

---
+++ C.NET_01_školení/WebAPI/Controllers/ContractsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContractsController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+
+        public IEnumerable<Contract> GetContracts()
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\marie\source\repos\NewRepo\C.NET_01_školení\personal_dataset\dataset.xml");
+            return dataset.SelectMany(x => x.Contracts);
+        }
+    }
+}
---
+++ C.NET_01_školení/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("GetAll")]
+        
+        public List<Person> GetPeople()
+        {
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\marie\source\repos\NewRepo\C.NET_01_školení\personal_dataset\dataset.xml");
+            return dataset;
+        }
+        [HttpGet("ByEmail/{email}")]
+        public Person GetbyEmail(string email)
+        { 
+            var dataset = Data.Serialization.LoadFromXML(@"C:\Users\marie\source\repos\NewRepo\C.NET_01_školení\personal_dataset\dataset.xml");
+            return dataset.Where(p => p.Email.ToLower() == email.ToLower()).FirstOrDefault();    
+        }
+    }
+}
